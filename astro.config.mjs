8626345be775cfb22f01f6e68f7ca39040bf5e35
Angela Yang <angela@dovetail.com>
2025-07-14 12:05:57 +1000

    Add adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import partytown from '@astrojs/partytown';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import { storyblok } from '@storyblok/astro';
-import { loadEnv } from 'vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { storyblok } from "@storyblok/astro";
+import { loadEnv } from "vite";
+import netlify from "@astrojs/netlify/functions";
 
-const env = loadEnv("", process.cwd(), 'STORYBLOK');
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown(), storyblok({
+  integrations: [
+    react(),
+    partytown(),
+    storyblok({
       accessToken: env.STORYBLOK_TOKEN,
+      livePreview: true,
+      bridge: true,
       components: {
         ["hero-section"]: "storybloks/HeroSection",
-        ["default-page"]: 'storybloks/Page',
+        ["default-page"]: "storybloks/Page",
       },
       apiOptions: {
-        region: 'eu',
+        region: "eu",
       },
-    })],
+    }),
+  ],
+  adapter: netlify({}),
+  output: "server",
   vite: {
-    plugins: [vanillaExtractPlugin()]
-  }
+    plugins: [vanillaExtractPlugin()],
+  },
 });
